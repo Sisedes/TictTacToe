4b99c51c0c2b65c35463015ce7c453d37b962e16
Kaan Toraman <kaan03tor@gmail.com>
2022-08-20 01:11:31 +0300

    files

---
+++ Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.player2Score = new System.Windows.Forms.Label();
+            this.player1Score = new System.Windows.Forms.Label();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.kare9 = new System.Windows.Forms.Button();
+            this.kare8 = new System.Windows.Forms.Button();
+            this.kare7 = new System.Windows.Forms.Button();
+            this.kare6 = new System.Windows.Forms.Button();
+            this.kare5 = new System.Windows.Forms.Button();
+            this.kare4 = new System.Windows.Forms.Button();
+            this.kare3 = new System.Windows.Forms.Button();
+            this.kare2 = new System.Windows.Forms.Button();
+            this.kare1 = new System.Windows.Forms.Button();
+            this.siyah4 = new System.Windows.Forms.Panel();
+            this.siyah3 = new System.Windows.Forms.Panel();
+            this.siyah2 = new System.Windows.Forms.Panel();
+            this.siyah1 = new System.Windows.Forms.Panel();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.mainPanel.Controls.Add(this.button1);
+            this.mainPanel.Controls.Add(this.player2Score);
+            this.mainPanel.Controls.Add(this.player1Score);
+            this.mainPanel.Controls.Add(this.player2);
+            this.mainPanel.Controls.Add(this.player1);
+            this.mainPanel.Controls.Add(this.restartButton);
+            this.mainPanel.Controls.Add(this.kare9);
+            this.mainPanel.Controls.Add(this.kare8);
+            this.mainPanel.Controls.Add(this.kare7);
+            this.mainPanel.Controls.Add(this.kare6);
+            this.mainPanel.Controls.Add(this.kare5);
+            this.mainPanel.Controls.Add(this.kare4);
+            this.mainPanel.Controls.Add(this.kare3);
+            this.mainPanel.Controls.Add(this.kare2);
+            this.mainPanel.Controls.Add(this.kare1);
+            this.mainPanel.Controls.Add(this.siyah4);
+            this.mainPanel.Controls.Add(this.siyah3);
+            this.mainPanel.Controls.Add(this.siyah2);
+            this.mainPanel.Controls.Add(this.siyah1);
+            this.mainPanel.Location = new System.Drawing.Point(1, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(500, 703);
+            this.mainPanel.TabIndex = 0;
+            this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPanel_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 624);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 27);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Reset Score";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // player2Score
+            // 
+            this.player2Score.AutoSize = true;
+            this.player2Score.Font = new System.Drawing.Font("Montserrat", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2Score.Location = new System.Drawing.Point(400, 567);
+            this.player2Score.Name = "player2Score";
+            this.player2Score.Size = new System.Drawing.Size(41, 40);
+            this.player2Score.TabIndex = 19;
+            this.player2Score.Text = "0";
+            // 
+            // player1Score
+            // 
+            this.player1Score.AutoSize = true;
+            this.player1Score.Font = new System.Drawing.Font("Montserrat", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1Score.Location = new System.Drawing.Point(52, 567);
+            this.player1Score.Name = "player1Score";
+            this.player1Score.Size = new System.Drawing.Size(41, 40);
+            this.player1Score.TabIndex = 18;
+            this.player1Score.Text = "0";
+            // 
+            // player2
+            // 
+            this.player2.Location = new System.Drawing.Point(387, 484);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(70, 70);
+            this.player2.TabIndex = 17;
+            this.player2.TabStop = false;
+            // 
+            // player1
+            // 
+            this.player1.Location = new System.Drawing.Point(39, 484);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(70, 70);
+            this.player1.TabIndex = 16;
+            this.player1.TabStop = false;
+            this.player1.Click += new System.EventHandler(this.player1_Click);
+            // 
+            // restartButton
+            // 
+            this.restartButton.Location = new System.Drawing.Point(165, 591);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(166, 27);
+            this.restartButton.TabIndex = 15;
+            this.restartButton.Text = "Restart";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // kare9
+            // 
+            this.kare9.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.kare9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kare9.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.kare9.Location = new System.Drawing.Point(337, 342);
+            this.kare9.Name = "kare9";
+            this.kare9.Size = new System.Drawing.Size(120, 120);
+            this.kare9.TabIndex = 14;
+            this.kare9.UseVisualStyleBackColor = false;
+            this.kare9.Click += new System.EventHandler(this.kare9_Click_1);
+            // 
+            // kare8
+            // 
+            this.kare8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kare8.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.kare8.Location = new System.Drawing.Point(188, 342);
+            this.kare8.Name = "kare8";
+            this.kare8.Size = new System.Drawing.Size(120, 120);
+            this.kare8.TabIndex = 13;
+            this.kare8.UseVisualStyleBackColor = true;
+            this.kare8.Click += new System.EventHandler(this.kare8_Click_1);
+            // 
+            // kare7
+            // 
+            this.kare7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kare7.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.kare7.Location = new System.Drawing.Point(39, 342);
+            this.kare7.Name = "kare7";
+            this.kare7.Size = new System.Drawing.Size(120, 120);
+            this.kare7.TabIndex = 12;
+            this.kare7.UseVisualStyleBackColor = true;
+            this.kare7.Click += new System.EventHandler(this.kare7_Click_1);
+            // 
+            // kare6
+            // 
+            this.kare6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kare6.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.kare6.Location = new System.Drawing.Point(337, 193);
+            this.kare6.Name = "kare6";
+            this.kare6.Size = new System.Drawing.Size(120, 120);
+            this.kare6.TabIndex = 11;
+            this.kare6.UseVisualStyleBackColor = true;
+            this.kare6.Click += new System.EventHandler(this.kare6_Click);
+            // 
+            // kare5
+            // 
+            this.kare5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kare5.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.kare5.Location = new System.Drawing.Point(188, 193);
+            this.kare5.Name = "kare5";
+            this.kare5.Size = new System.Drawing.Size(120, 120);
+            this.kare5.TabIndex = 10;
+            this.kare5.UseVisualStyleBackColor = true;
+            this.kare5.Click += new System.EventHandler(this.kare5_Click);
+            // 
+            // kare4
+            // 
+            this.kare4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kare4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.kare4.Location = new System.Drawing.Point(39, 193);
+            this.kare4.Name = "kare4";
+            this.kare4.Size = new System.Drawing.Size(120, 120);
+            this.kare4.TabIndex = 9;
+            this.kare4.UseVisualStyleBackColor = true;
+            this.kare4.Click += new System.EventHandler(this.kare4_Click);
+            // 
+            // kare3
+            // 
+            this.kare3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.kare3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kare3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.kare3.Location = new System.Drawing.Point(337, 44);
+            this.kare3.Name = "kare3";
+            this.kare3.Size = new System.Drawing.Size(120, 120);
+            this.kare3.TabIndex = 8;
+            this.kare3.UseVisualStyleBackColor = false;
+            this.kare3.Click += new System.EventHandler(this.kare3_Click);
+            // 
+            // kare2
+            // 
+            this.kare2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kare2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.kare2.Location = new System.Drawing.Point(188, 44);
+            this.kare2.Name = "kare2";
+            this.kare2.Size = new System.Drawing.Size(120, 120);
+            this.kare2.TabIndex = 7;
+            this.kare2.UseVisualStyleBackColor = true;
+            this.kare2.Click += new System.EventHandler(this.kare2_Click);
+            // 
+            // kare1
+            // 
+            this.kare1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.kare1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kare1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.kare1.Location = new System.Drawing.Point(39, 44);
+            this.kare1.Name = "kare1";
+            this.kare1.Size = new System.Drawing.Size(120, 120);
+            this.kare1.TabIndex = 0;
+            this.kare1.UseVisualStyleBackColor = true;
+            this.kare1.Click += new System.EventHandler(this.kare1_Click_1);
+            // 
+            // siyah4
+            // 
+            this.siyah4.BackColor = System.Drawing.SystemColors.ControlText;
+            this.siyah4.Location = new System.Drawing.Point(39, 319);
+            this.siyah4.Name = "siyah4";
+            this.siyah4.Size = new System.Drawing.Size(418, 17);
+            this.siyah4.TabIndex = 6;
+            // 
+            // siyah3
+            // 
+            this.siyah3.BackColor = System.Drawing.Color.Black;
+            this.siyah3.Location = new System.Drawing.Point(39, 170);
+            this.siyah3.Name = "siyah3";
+            this.siyah3.Size = new System.Drawing.Size(418, 17);
+            this.siyah3.TabIndex = 5;
+            // 
+            // siyah2
+            // 
+            this.siyah2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.siyah2.Location = new System.Drawing.Point(314, 44);
+            this.siyah2.Name = "siyah2";
+            this.siyah2.Size = new System.Drawing.Size(17, 418);
+            this.siyah2.TabIndex = 4;
+            // 
+            // siyah1
+            // 
+            this.siyah1.BackColor = System.Drawing.Color.Black;
+            this.siyah1.Location = new System.Drawing.Point(165, 44);
+            this.siyah1.Name = "siyah1";
+            this.siyah1.Size = new System.Drawing.Size(17, 418);
+            this.siyah1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 703);
+            this.Controls.Add(this.mainPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Panel siyah4;
+        private System.Windows.Forms.Panel siyah3;
+        private System.Windows.Forms.Panel siyah2;
+        private System.Windows.Forms.Panel siyah1;
+        private System.Windows.Forms.Button kare9;
+        private System.Windows.Forms.Button kare8;
+        private System.Windows.Forms.Button kare7;
+        private System.Windows.Forms.Button kare6;
+        private System.Windows.Forms.Button kare5;
+        private System.Windows.Forms.Button kare4;
+        private System.Windows.Forms.Button kare3;
+        private System.Windows.Forms.Button kare2;
+        private System.Windows.Forms.Button kare1;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.Label player2Score;
+        private System.Windows.Forms.Label player1Score;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        static int say = 0;
+        bool x1, x2, x3, x4, x5, x6, x7, x8, x9 = false;
+        bool o1, o2, o3, o4, o5, o6, o7, o8, o9 = false;
+        bool bitti = false;
+        static int p1, p2 = 0;
+        
+
+        private void player1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p1 = 0;p2 = 0;
+            player1Score.Text = p1.ToString();
+            player2Score.Text = p2.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        #region click events
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            x1 = false;x2 = false;x3 = false;x4 = false;x5 = false;x6 = false;x7 = false;x8 = false;x9 = false;
+            o1 = false;o2 = false;o3 = false;o4 = false;o5= false;o6 = false;o7 = false;o8 = false;x9 = false;
+            bitti = false;
+            say= 0;
+            kare1.BackgroundImage = null;kare1.Enabled = true;
+            kare2.BackgroundImage = null;kare2.Enabled = true;
+            kare3.BackgroundImage = null;kare3.Enabled = true;
+            kare4.BackgroundImage = null;kare4.Enabled = true;
+            kare5.BackgroundImage = null;kare5.Enabled = true;
+            kare6.BackgroundImage = null;kare6.Enabled = true;
+            kare7.BackgroundImage = null;kare7.Enabled = true;
+            kare8.BackgroundImage = null;kare8.Enabled = true;
+            kare9.BackgroundImage = null;kare9.Enabled = true;
+
+        }
+        
+        private void kare9_Click_1(object sender, EventArgs e)
+        {
+            if (say % 2 == 1)
+            {
+                kare9.BackgroundImage = Properties.Resources.o;
+                kare9.BackgroundImageLayout = ImageLayout.Stretch;
+                o9 = true;
+                okazandi();
+                say++;
+                kare9.Enabled = false;
+            }
+            else
+            {
+                kare9.BackgroundImage = Properties.Resources.x;
+                kare9.BackgroundImageLayout = ImageLayout.Stretch;
+                x9 = true;
+                xkazandi();
+                say++;
+                kare9.Enabled = false;
+            }
+        }
+
+        private void kare8_Click_1(object sender, EventArgs e)
+        {
+            if (say % 2 == 1)
+            {
+                kare8.BackgroundImage = Properties.Resources.o;
+                kare8.BackgroundImageLayout = ImageLayout.Stretch;
+                o8 = true;
+                okazandi();
+                say++;
+                kare8.Enabled = false;
+            }
+            else
+            {
+                kare8.BackgroundImage = Properties.Resources.x;
+                kare8.BackgroundImageLayout = ImageLayout.Stretch;
+                x8 = true;
+                xkazandi();
+                say++;
+                kare8.Enabled = false;
+            }
+        }
+
+        private void kare7_Click_1(object sender, EventArgs e)
+        {
+            if (say % 2 == 1)
+            {
+                kare7.BackgroundImage = Properties.Resources.o;
+                kare7.BackgroundImageLayout = ImageLayout.Stretch;
+                o7 = true;
+                okazandi();
+                say++;
+                kare7.Enabled = false;
+            }
+            else
+            {
+                kare7.BackgroundImage = Properties.Resources.x;
+                kare7.BackgroundImageLayout = ImageLayout.Stretch;
+                x7 = true;
+                xkazandi();
+                say++;
+                kare7.Enabled = false;
+            }
+        }
+
+        private void kare6_Click(object sender, EventArgs e)
+        {
+            if (say % 2 == 1)
+            {
+                kare6.BackgroundImage = Properties.Resources.o;
+                kare6.BackgroundImageLayout = ImageLayout.Stretch;
+                o6 = true;
+                okazandi();
+                say++;
+                kare6.Enabled = false;
+
+            }
+            else
+            {
+                kare6.BackgroundImage = Properties.Resources.x;
+                kare6.BackgroundImageLayout = ImageLayout.Stretch;
+                x6 = true;
+                xkazandi();
+                say++;
+                kare6.Enabled=false;
+            }
+        }
+
+        private void kare5_Click(object sender, EventArgs e)
+        {
+            if (say % 2 == 1)
+            {
+                kare5.BackgroundImage = Properties.Resources.o;
+                kare5.BackgroundImageLayout = ImageLayout.Stretch;
+                o5 = true;
+                okazandi();
+                say++;
+                kare5.Enabled = false;
+            }
+            else
+            {
+                kare5.BackgroundImage = Properties.Resources.x;
+                kare5.BackgroundImageLayout = ImageLayout.Stretch;
+                x5 = true;
+                xkazandi();
+                say++;
+                kare5.Enabled = false;
+            }
+        }
+
+        private void kare4_Click(object sender, EventArgs e)
+        {
+            if (say % 2 == 1)
+            {
+                kare4.BackgroundImage = Properties.Resources.o;
+                kare4.BackgroundImageLayout = ImageLayout.Stretch;
+                o4 = true;
+                okazandi();
+                say++;
+                kare4.Enabled = false;
+            }
+            else
+            {
+                kare4.BackgroundImage = Properties.Resources.x;
+                kare4.BackgroundImageLayout = ImageLayout.Stretch;
+                x4 = true;
+                xkazandi();
+                say++;
+                kare4.Enabled = false;
+            }
+        }
+
+        private void kare3_Click(object sender, EventArgs e)
+        {
+            if (say % 2 == 1)
+            {
+                kare3.BackgroundImage = Properties.Resources.o;
+                kare3.BackgroundImageLayout = ImageLayout.Stretch;
+                o3 = true;
+                okazandi();
+                say++;
+                kare3.Enabled = false;
+            }
+            else
+            {
+                kare3.BackgroundImage = Properties.Resources.x;
+                kare3.BackgroundImageLayout = ImageLayout.Stretch;
+                x3 = true;
+                xkazandi();
+                say++;
+                kare3.Enabled=false;
+            }
+        }
+
+        private void kare2_Click(object sender, EventArgs e)
+        {
+            if (say % 2 == 1)
+            {
+                kare2.BackgroundImage = Properties.Resources.o;
+                kare2.BackgroundImageLayout = ImageLayout.Stretch;
+                o2 = true;
+                okazandi();
+                say++;
+                kare2.Enabled = false;
+            }
+            else
+            {
+                kare2.BackgroundImage = Properties.Resources.x;
+                kare2.BackgroundImageLayout = ImageLayout.Stretch;
+                x2 = true;
+                xkazandi();
+                say++;
+                kare2.Enabled = false;
+            }
+        }
+
+        private void kare1_Click_1(object sender, EventArgs e)
+        {
+            if (say % 2 == 1)
+            {
+                kare1.BackgroundImage = Properties.Resources.o;
+                kare1.BackgroundImageLayout = ImageLayout.Stretch;
+                o1 = true;
+                okazandi();
+                say++;
+                kare1.Enabled = false;
+            }
+            else
+            {
+                kare1.BackgroundImage = Properties.Resources.x;
+                kare1.BackgroundImageLayout = ImageLayout.Stretch;
+                x1 = true;
+                say++;
+                xkazandi();
+                kare1.Enabled = false;
+            }
+
+        }
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+            player1.BackgroundImage = Properties.Resources.P1;
+            player1.BackgroundImageLayout = ImageLayout.Stretch;
+            player2.BackgroundImage=Properties.Resources.P2;
+            player2.BackgroundImageLayout = ImageLayout.Stretch;
+           
+            
+        }
+
+        private void mainPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        #region win events
+        private void xkazandi()
+        {
+            if (x1 && x2 && x3 == true)
+            {
+                MessageBox.Show("First Player Win");
+                p1++;
+                player1Score.Text = p1.ToString();
+                bitti = true;
+            }
+            else if(x1 && x5 && x9 == true)
+            {
+                MessageBox.Show("First Player Win");
+                p1++;
+                player1Score.Text = p1.ToString();
+                bitti = true;
+            }
+            else if(x1&&x4 && x7 == true)
+            {
+                MessageBox.Show("First Player Win");
+                p1++;
+                player1Score.Text = p1.ToString();
+                bitti = true;
+            }
+            else if(x2&&x5&&x8 == true)
+            {
+                MessageBox.Show("First Player Win");
+                p1++;
+                player1Score.Text = p1.ToString();
+                bitti = true;
+            }
+            else if (x3 && x5 && x7 == true) { MessageBox.Show("First Player Win"); p1++; player1Score.Text = p1.ToString(); bitti = true; }
+            else if(x3 && x6 && x9 == true) { MessageBox.Show("First Player Win"); p1++; player1Score.Text = p1.ToString(); bitti = true; }
+            else if (x4 && x5 && x6 == true) { MessageBox.Show("First Player Win"); p1++; player1Score.Text = p1.ToString(); bitti = true;}
+            else if (x7 && x8 && x9 == true) { MessageBox.Show("First Player Win"); p1++; player1Score.Text = p1.ToString(); bitti = true;}
+            
+        }
+        private void okazandi()
+        {
+            if (o1 && o2 && o3 == true)
+            {
+                MessageBox.Show("Second Player Win");
+                p2++;
+                player2Score.Text = p2.ToString();
+                bitti = true;
+            }
+            else if (o1 && o5 && o9 == true)
+            {
+                MessageBox.Show("Second Player Win");
+                p2++;
+                player2Score.Text = p2.ToString();
+                bitti = true;
+            }
+            else if (o1 && o4 && o7 == true)
+            {
+                MessageBox.Show("Second Player Win");
+                p2++;
+                player2Score.Text = p2.ToString();
+                bitti = true;
+            }
+            else if (o2 && o5 && o8 == true)
+            {
+                MessageBox.Show("Second Player Win");
+                p2++;
+                player2Score.Text = p2.ToString();
+                bitti = true;
+            }
+            else if (o3 && o5 && o7 == true) { MessageBox.Show("Second Player Win"); p2++; player2Score.Text = p2.ToString(); bitti = true; }
+            else if (o3 && o6 && o9 == true) { MessageBox.Show("Second Player Win"); p2++; player2Score.Text = p2.ToString(); bitti = true; }
+            else if (o4 && o5 && o6 == true) { MessageBox.Show("Second Player Win"); p2++; player2Score.Text = p2.ToString(); bitti = true; }
+            else if (o7 && o8 && o9 == true) { MessageBox.Show("Second Player Win"); p2++; player2Score.Text = p2.ToString(); bitti = true; }
+           
+
+        }
+        #endregion
+
+
+    }
+}
